cfc055ae6d6409ce5f277409552fedb68af09b52
depresstrogen <rpowe013@uottawa.ca>
2024-05-14 23:59:04 -0400

    Test ./

--- apps/website/website.js
+++ apps/website/website.js
@@ -4,7 +4,7 @@ function websiteRun() {
     let websiteWindow = makeWindow(10, 30, height, width, "Welcome to Emma's Website");
     let tempDiv = document.createElement("div");
     
-    tempDiv.innerHTML = '<object type="text/html" data="/apps/website/index.html" height = "' + height + '" width = "' + width + '"></object>';
+    tempDiv.innerHTML = '<object type="text/html" data="./apps/website/index.html" height = "' + height + '" width = "' + width + '"></object>';
     tempDiv.style.height = height - 22 + "px";
     tempDiv.style.width = width + "px";
     websiteWindow.appendChild(tempDiv);
